0b3093d2df3f93b1f9b1b607a4b77bf2f3ad63a8
Tavion20 <tavionalric@gmail.com>
2022-10-13 22:58:06 +0530

    Textarea

--- src/MainPage.java
+++ src/MainPage.java
@@ -11,6 +11,10 @@ public class MainPage implements ActionListener {
     JButton stbutton = new JButton("Start");
     JLabel timelabel = new JLabel();
     JButton button = new JButton("Finish");
+    ImageIcon start = new ImageIcon("start.png");
+    JLabel st = new JLabel(start);
+    JTextField textField2 = new JTextField();
+    JTextArea textArea = new JTextArea();
     JButton bbutton = new JButton("<--");
     int elapsedtime=0;
     int seconds=0;
@@ -65,7 +69,7 @@ public class MainPage implements ActionListener {
         timelabel.setOpaque(true);
         timelabel.setHorizontalAlignment(JTextField.CENTER);
         timelabel.setText(hours_string+":"+minutes_string+":"+seconds_string);
-        timelabel.setBounds(1200,50,200,100);
+        timelabel.setBounds(1200,10,200,100);
         timelabel.setFont(new Font("Verdana",Font.PLAIN,35));
         timelabel.setBorder(BorderFactory.createBevelBorder(1));
         bglabel.add(timelabel);
@@ -73,6 +77,21 @@ public class MainPage implements ActionListener {
         stbutton.setFocusable(false);
         stbutton.addActionListener(this);
         bglabel.add(stbutton);
+        //TextArea
+        bglabel.add(textArea);
+        textArea.setBounds(790,120,700,500);
+        textArea.setAlignmentY(0.0f);
+        textArea.setVisible(true);
+        textArea.setBackground(Color.darkGray);
+        textArea.setFont(new Font("Verdana",Font.PLAIN,20));
+        textArea.setForeground(Color.white);
+        textArea.setCaretColor(Color.white);
+        textArea.setText("Hello CodeSpeeder!!\n\nMaster your Typing skills here...\n\n");
+        //TextField
+        bglabel.add(textField2);
+        textField2.setVisible(true);
+        textField2.setBounds(50,80,650,650);
+        textField2.setAlignmentY(0.0f);
     }
     @Override
     public void actionPerformed(ActionEvent e){
